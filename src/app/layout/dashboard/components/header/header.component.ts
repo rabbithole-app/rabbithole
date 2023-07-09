83d9104252fe007d4c8db0f3fbc135aebdcaa0f5
Khalik Faradzhli <khalik.faradzhli@mail.ru>
2023-07-09 19:20:41 +0500

    fix(layout): fixed burger button

--- src/app/layout/dashboard/components/header/header.component.ts
+++ src/app/layout/dashboard/components/header/header.component.ts
@@ -1,12 +1,12 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Signal, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { TranslocoModule } from '@ngneat/transloco';
-import { RxPush } from '@rx-angular/template/push';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 import { SETTINGS_RX_STATE } from '@core/stores';
 import { addFASvgIcons } from '@core/utils';
@@ -16,7 +16,7 @@ import { addFASvgIcons } from '@core/utils';
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [RouterModule, MatButtonModule, MatIconModule, RxPush, TranslocoModule],
+    imports: [RouterModule, MatButtonModule, MatIconModule, TranslocoModule],
     standalone: true
 })
 export class HeaderComponent {
@@ -25,6 +25,7 @@ export class HeaderComponent {
     isCompact$: Observable<boolean> = this.breakpointObserver
         .observe(['(min-width: 960px)', '(max-width: 959.98px)'])
         .pipe(map(({ breakpoints: { '(max-width: 959.98px)': isCompact } }) => isCompact));
+    isCompact: Signal<boolean> = toSignal(this.isCompact$, { initialValue: false });
 
     toggleSidebar() {
         this.settingsState.set('sidebarMode', ({ sidebarMode }) => (sidebarMode === 'compact' ? 'full' : 'compact'));
